f99dba88e781f4c1898f6969fbc3d0471ba182da
karl baldelomar <karlthegreat18@gmail.com>
2024-05-23 04:37:48 +0800

    blatantly copy all files from @sayem314/react-native-keep-awake

--- src/StayinAlive.jsx
+++ src/StayinAlive.jsx
@@ -1,13 +1,8 @@
-import { createElement, useCallback } from "react";
+import { createElement } from "react";
 
-import { Badge } from "./components/Badge";
+// import KeepAwake from "@sayem314/react-native-keep-awake";
+import KeepAwake from "./components/KeepAwake";
 
-export function StayinAlive({ value, style, onClick }) {
-    const onClickHandler = useCallback(() => {
-        if (onClick && onClick.canExecute && !onClick.isExecuting) {
-            onClick.execute();
-        }
-    }, [onClick]);
-
-    return <Badge style={style} onClick={onClickHandler} value={value?.displayValue || "Default"} />;
+export function StayinAlive() {
+    return <KeepAwake />;
 }
